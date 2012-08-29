312b25de8490e0ac42895296c8ac1774cd167957
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-08-29 11:21:36 -0400

    Changed wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,6 @@
 import os
 import sys
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'flaggy')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'flaggy.settings'
-
 import django.core.handlers.wsgi
-
-djangoapplication = django.core.handlers.wsgi.WSGIHandler()
-
-def application(environ, start_response):
-    if 'SCRIPT_NAME' in environ:
-        del environ['SCRIPT_NAME']
-    return djangoapplication(environ, start_response)
+application = django.core.handlers.wsgi.WSGIHandler()
