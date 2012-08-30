286c7ea3e026f204741b9a80da8b3022b93fe663
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-08-30 16:07:16 -0400

    verifyExistence

--- flaggy/checkins/views.py
+++ flaggy/checkins/views.py
@@ -33,8 +33,7 @@ def addUser(request):
 		l_n = request.GET.get('lname')
 		fb_id = request.GET.get('fb_id')
 
-		if(f_n != None and l_n != None and fb_id != None 
-			and !verifyExistence(User, "fb_id", fb_id)):
+		if(f_n != None and l_n != None and fb_id != None and !verifyExistence(User, "fb_id", fb_id)):
 			__addUser(f_n,l_n,fb_id, 0000)
 			return HttpResponseRedirect('/userAdded/')
 		else:
