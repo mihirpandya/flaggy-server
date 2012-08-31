ff889ed687e56c6f86df43624ade9c706530e2cc
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-08-30 21:28:32 -0400

    Added email and date parameters to addUser

--- flaggy/checkins/views.py
+++ flaggy/checkins/views.py
@@ -25,7 +25,7 @@ def verifyUser(obj, value):
 
 def __addUser(f_n, l_n, fb, twitter, email):
 	d = str(datetime.date(datetime.now()))
-	u = User(fname=f_n, lname=l_n, fb_id=fb, twitter_id=twitter, email=email,date=d)
+	u = User(fname=f_n, lname=l_n, fb_id=fb, twitter_id=twitter, email=email)
 	u.save()
 	return None
 
