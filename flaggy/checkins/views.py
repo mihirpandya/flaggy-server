de4914fd7b79800a7d69a7b4109d7555bac04464
Ilter Canberk <ilteriscanberk@gmail.com>
2012-08-31 22:22:51 -0400

    Json Dump

--- flaggy/checkins/views.py
+++ flaggy/checkins/views.py
@@ -58,9 +58,9 @@ def followers(request):
 		res = __followers(u_id)
 		## need to get the data as dictionary(or object if you wanna call it)
 		## and then serialize it and render it that way
-		followers = {'a': 1, 'b': 2, 'c': 3}
-		data = serializers.serialize('json', followers)
-		return HttpResponse(data, mimetype='application/json')
+		##followers = {'a': 1, 'b': 2, 'c': 3}
+		##data = serializers.serialize('json', followers)
+		return HttpResponse(simplejson.dump(followers), mimetype='application/json')
 	else: 
 		return HttpResponseRedirect('/notGETmethod/')
 
