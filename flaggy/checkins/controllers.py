80ad053d6a96fd0cdd10d4352d58dc6eaf0322ba
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-09-06 11:10:00 -0400

    Email bug fixed.

--- flaggy/checkins/controllers.py
+++ flaggy/checkins/controllers.py
@@ -51,12 +51,17 @@ def __add_follow(follower, followed):
 			approve_url = "http://flaggy-mihirmp.dotcloud.com/approve_request?k="+k
 			print approve_url
 			print f_ed.email
-			mail_success = send_mail(f_er.fname+" wants to follow you on Flaggy App!", approve_url, 'firepent@hotmail.com', [f_ed.email], fail_silently=False)
 
-			if(mail_success):
-				return "Request sent to "+f_ed.fname
-			else:
-				return "Failed to send request "+f_ed.fname
+			try:
+				mail_success = send_mail(f_er.fname+" wants to follow you on Flaggy App!", approve_url, 'firepent@hotmail.com', [f_ed.email], fail_silently=False)
+					
+				if(mail_success):
+					return "Request sent to "+f_ed.fname
+				else:
+					return "Failed to send request "+f_ed.fname
+
+			except django.core.mail.SMTPServerDisconnected:
+				return "SMTP Server Disconnected unexpectedly."
 
 	except User.DoesNotExist:
 		return "User does not exist."
