613ebb6e63b7a2b3f8f3a8b1b09918a440064bfa
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-09-07 14:54:26 -0400

    Removed checkins app

--- flaggy/flaggy/settings.py
+++ flaggy/flaggy/settings.py
@@ -140,7 +140,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'checkins',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
