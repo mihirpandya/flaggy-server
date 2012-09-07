8baa4a021aed37b57485583f10c94cf945360f20
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-09-07 17:11:58 -0400

    Added JSON success and error messages.

--- flaggy/api/views.py
+++ flaggy/api/views.py
@@ -17,109 +17,115 @@ def hello_view(request):
     return HttpResponse(t.render(c))
 
 def add_user(request):
-	if request.method == 'GET':
-		
-		f_n = request.GET.get('fname')
-		l_n = request.GET.get('lname')
-		fb_id = request.GET.get('fb_id')
-		email = request.GET.get('email')
+    if request.method == 'GET':
+        
+        f_n = request.GET.get('fname')
+        l_n = request.GET.get('lname')
+        fb_id = request.GET.get('fb_id')
+        email = request.GET.get('email')
 
-		res = { }
-		checkin_user = { }
+        res = { }
+        checkin_user = { }
 
-		if (verify_user(fb_id)):
-			u = User.objects.get(fb_id=fb_id)
+        if (verify_user(fb_id)):
+            u = User.objects.get(fb_id=fb_id)
 
-			c = CheckIn.objects.filter(u_id_id = u.u_id)
+            c = CheckIn.objects.filter(u_id_id = u.u_id)
 
-			if(len(c)):
-				last_checkin = c[(len(c)-1)]
+            if(len(c)):
+                last_checkin = c[(len(c)-1)]
 
-				checkin_user["longitude"] = int(last_checkin.longitude)
-				checkin_user["latitude"] = int(last_checkin.latitude)
-				checkin_user["when"] = str(last_checkin.when)
-				checkin_user["comment"] = str(last_checkin.comment)
-				
-			res["status"] = 2
-			res["last_checkin"] = checkin_user
-			## Add the last checkin location here
-			res["u_id"] = str(u.u_id)
-			## We should return friends (you mean followers) if the user already exists ##
-			res["followers"] = __followers(u.u_id)
+                checkin_user["longitude"] = int(last_checkin.longitude)
+                checkin_user["latitude"] = int(last_checkin.latitude)
+                checkin_user["when"] = str(last_checkin.when)
+                checkin_user["comment"] = str(last_checkin.comment)
 
-		elif (not(empty_str(f_n)) and not(empty_str(l_n)) and not(empty_str(fb_id))):
-			res["status"] = 1
-			res["u_id"] = __add_user(f_n,l_n,fb_id, 0000, email)
-		else:
-			res["status"] = 0
-		
-		return HttpResponse(dumps(res), mimetype='application/json')
+            res["status"] = 2
+            res["last_checkin"] = checkin_user
+            ## Add the last checkin location here
+            res["u_id"] = str(u.u_id)
+            ## We should return friends (you mean followers) if the user already exists ##
+            res["followers"] = __followers(u.u_id)
+
+        elif (not(empty_str(f_n)) and not(empty_str(l_n)) and not(empty_str(fb_id))):
+            res["status"] = 1
+            res["u_id"] = __add_user(f_n,l_n,fb_id, 0000, email)
+        else:
+            res["status"] = 0
+
+        return HttpResponse(dumps(res), mimetype='application/json')
 
 def add_follow(request):
-	if request.method == 'GET':
-		follower = request.GET.get('f_er')
-		followed = request.GET.get('f_ed')
+    if request.method == 'GET':
+        follower = request.GET.get('f_er')
+        followed = request.GET.get('f_ed')
 
-		if(follower != None and followed != None):
-			res = __add_follow(follower, followed)
-			return HttpResponse(dumps(res), mimetype='application/json')
-		else:
-		    err = error("Error. Did not find either user.") 
-		    return HttpResponse(dumps(err), mimetype='application/json')
+        if(follower != None and followed != None):
+            res = __add_follow(follower, followed)
+            return HttpResponse(dumps(res), mimetype='application/json')
+        else:
+            err = error("Error. Did not find either user.") 
+            return HttpResponse(dumps(err), mimetype='application/json')
 
-	else:
-		err = error("No request received.")
-		return HttpResponse(dumps(err), mimetype='application/json')
+    else:
+        err = error("No request received.")
+        return HttpResponse(dumps(err), mimetype='application/json')
 
 def approve_request(request):
-	if request.method == 'GET':
-		key = request.GET.get('k')
+    if request.method == 'GET':
+        key = request.GET.get('k')
 
-		res = __approve_request(key)
+        res = __approve_request(key)
 
-		return HttpResponse(res, mimetype='application/json')
+        return HttpResponse(dumps(res), mimetype='application/json')
 
 def unfollow(request):
-	if request.method == 'GET':
-		f_er = request.GET.get('f_er')
-		f_ed = request.GET.get('f_ed')
+    if request.method == 'GET':
+        f_er = request.GET.get('f_er')
+        f_ed = request.GET.get('f_ed')
 
-		res = __unfollow(f_er, f_ed)
+        res = __unfollow(f_er, f_ed)
 
-		return HttpResponse(dumps(res), mimetype='application/json')
+        return HttpResponse(dumps(res), mimetype='application/json')
 
 def followers(request):
-	if request.method == 'GET':
-		u_id = request.GET.get('u_id')
-		res = __followers(u_id)
+    if request.method == 'GET':
+        u_id = request.GET.get('u_id')
+        res = __followers(u_id)
 
-		if len(res):
-			return HttpResponse(dumps(res), mimetype='application/json')
-		else:
-			return HttpResponse("No followers", mimetype='application/json')
-	else: 
-		return HttpResponse("No request received.", mimetype='application/json')
+        if len(res):
+            return HttpResponse(dumps(res), mimetype='application/json')
+        else:
+            res = success("No followers")
+            return HttpResponse(dumps(res), mimetype='application/json')
+    else:
+        err = error("No request received.")
+        return HttpResponse(dumps(err), mimetype='application/json')
 
 def following(request):
-	if request.method == 'GET':
-		u_id = request.GET.get('u_id')
-		res = __following(u_id)
-		if len(res):
-			return HttpResponse(dumps(res), mimetype='application/json')
-		else:
-			return HttpResponse("Following no one", mimetype='application/json')
-	else: 
-		return HttpResponse("No request received.", mimetype='application/json')		
+    if request.method == 'GET':
+        u_id = request.GET.get('u_id')
+        res = __following(u_id)
+        if len(res):
+            return HttpResponse(dumps(res), mimetype='application/json')
+        else:
+            res = success("Following no one")
+            return HttpResponse(dumps(res), mimetype='application/json')
+    else:
+        err = error("No request received.") 
+        return HttpResponse(dumps(res), mimetype='application/json')        
 
 def check_in(request):
-	if request.method == 'GET':
-		u_id = request.GET.get('u_id')
-		lat = request.GET.get('lat')
-		lon = request.GET.get('long')
-		comm = request.GET.get('comm')
+    if request.method == 'GET':
+        u_id = request.GET.get('u_id')
+        lat = request.GET.get('lat')
+        lon = request.GET.get('long')
+        comm = request.GET.get('comm')
 
-		__check_in(lon, lat, u_id, comm)
-		return HttpResponse("Checked In", mimetype='application/json')
-	else: 
-		return HttpResponse("No request received.", mimetype='application/json')
+        res = __check_in(lon, lat, u_id, comm)
+
+        return HttpResponse(dumps(res), mimetype='application/json')
+    else:
+        err = error("No request received") 
+        return HttpResponse(dumps(res), mimetype='application/json')
 
