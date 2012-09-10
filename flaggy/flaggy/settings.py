0b6b21e149ae96c8e929b0881faf6949e5e75f12
Burak Bekdemir <bbekdemir@gmail.com>
2012-09-10 19:43:56 -0400

    loader for celery

--- flaggy/flaggy/settings.py
+++ flaggy/flaggy/settings.py
@@ -3,6 +3,9 @@
 import os
 import pwd
 import json
+import djcelery
+
+djcelery.setup_loader()
 
 settings_dir = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))
