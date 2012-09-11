6379de952c493ff2f685931b70cdf5b0ebb7f862
Burak Bekdemir <bbekdemir@gmail.com>
2012-09-11 00:21:08 -0400

    lots of style changes according to PEP8

--- flaggy/doppio/api/views.py
+++ flaggy/doppio/api/views.py
@@ -1,12 +1,10 @@
 from doppio.models import *
 from doppio.api.controllers import __add_user, __add_follow, __unfollow, __approve_request, __followers, __following, __check_in, verify_user, success, error, empty_str, last_check_in
-from django.utils import simplejson
-from json import loads, dumps
-from django.core import serializers
-
+from json import dumps
 from django.template import Context, loader
 from datetime import datetime
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
+
 
 def hello_view(request):
     """ Simple Hello World View """
@@ -16,16 +14,16 @@ def hello_view(request):
     })
     return HttpResponse(t.render(c))
 
+
 def add_user(request):
     if request.method == 'GET':
-        
         f_n = request.GET.get('fname')
         l_n = request.GET.get('lname')
         fb_id = request.GET.get('fb_id')
         email = request.GET.get('email')
 
-        res = { }
-        checkin_user = { }
+        res = {}
+        checkin_user = {}
 
         if (verify_user(fb_id)):
             u = User.objects.get(fb_id=fb_id)
@@ -44,11 +42,12 @@ def add_user(request):
 
         elif (not(empty_str(f_n)) and not(empty_str(l_n)) and not(empty_str(fb_id))):
             res["status"] = 1
-            res["u_id"] = __add_user(f_n,l_n,fb_id, 0000, email)
+            res["u_id"] = __add_user(f_n, l_n, fb_id, 0000, email)
         else:
             res["status"] = 0
 
         return HttpResponse(dumps(res), mimetype='application/json')
+
 
 # accepts f_er, fb_ed, email_ed
 def add_follow(request):
@@ -57,17 +56,17 @@ def add_follow(request):
         followed_fb = request.GET.get('fb_ed')
         followed_email = request.GET.get('email_ed')
 
-
         if(follower is not None and followed_fb is not None and followed_email is not None):
             res = __add_follow(follower, followed_fb, followed_email)
             return HttpResponse(dumps(res), mimetype='application/json')
         else:
-            err = error("Error. Did not find either user.") 
+            err = error("Error. Did not find either user.")
             return HttpResponse(dumps(err), mimetype='application/json')
 
     else:
         err = error("No request received.")
         return HttpResponse(dumps(err), mimetype='application/json')
+
 
 def approve_request(request):
     if request.method == 'GET':
@@ -77,6 +76,7 @@ def approve_request(request):
 
         return HttpResponse(dumps(res), mimetype='application/json')
 
+
 def unfollow(request):
     if request.method == 'GET':
         f_er = request.GET.get('f_er')
@@ -85,6 +85,7 @@ def unfollow(request):
         res = __unfollow(f_er, f_ed)
 
         return HttpResponse(dumps(res), mimetype='application/json')
+
 
 def followers(request):
     if request.method == 'GET':
@@ -100,6 +101,7 @@ def followers(request):
         err = error("No request received.")
         return HttpResponse(dumps(err), mimetype='application/json')
 
+
 def following(request):
     if request.method == 'GET':
         u_id = request.GET.get('u_id')
@@ -110,8 +112,9 @@ def following(request):
             res = success("Following no one")
             return HttpResponse(dumps(res), mimetype='application/json')
     else:
-        err = error("No request received.") 
-        return HttpResponse(dumps(res), mimetype='application/json')        
+        err = error("No request received.")
+        return HttpResponse(dumps(err), mimetype='application/json')
+
 
 def check_in(request):
     if request.method == 'GET':
@@ -119,11 +122,8 @@ def check_in(request):
         lat = request.GET.get('lat')
         lon = request.GET.get('long')
         comm = request.GET.get('comm')
-
         res = __check_in(lon, lat, u_id, comm)
-
         return HttpResponse(dumps(res), mimetype='application/json')
     else:
-        err = error("No request received") 
-        return HttpResponse(dumps(res), mimetype='application/json')
-
+        err = error("No request received")
+        return HttpResponse(dumps(err), mimetype='application/json')
