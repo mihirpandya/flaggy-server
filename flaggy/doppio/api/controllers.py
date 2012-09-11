b1de491d334d2b88f7b1d26a95a8d077404b76ed
Burak Bekdemir <bbekdemir@gmail.com>
2012-09-11 00:38:25 -0400

    syntax changes

--- flaggy/doppio/api/controllers.py
+++ flaggy/doppio/api/controllers.py
@@ -7,14 +7,18 @@ from django.core.mail import send_mail
 
 
 def __add_user(f_n, l_n, fb, twitter, email):
-    d = datetime.now()
     try:
-        u = User(fname=f_n, lname=l_n, fb_id=fb, twitter_id=twitter, email=email, date_joined=d)
+        u = User(
+            fname=f_n,
+            lname=l_n,
+            fb_id=fb,
+            twitter_id=twitter,
+            email=email,
+            date_joined=datetime.now()
+            )
         u.save()
         send_mail("Welcome to Flaggy App!", "Thank you for joining Flaggy App!", 'firepent@hotmail.com', [u.email], fail_silently=False)
-
         return str(u.pk)
-
     except:
         return 0
 
@@ -22,21 +26,20 @@ def __add_user(f_n, l_n, fb, twitter, email):
 def __add_follow(follower, followed_fb, followed_email):
     try:
         f_er = User.objects.get(pk=follower)
-
-        if(verify_user(followed_fb)):
+        if verify_user(followed_fb):
             # Send email to existing user. Add to follow pending table
 
             f_ed = User.objects.get(fb_id=followed_fb)
-            k = hashlib.sha224(str(f_er.pk) + "&" + str(f_ed.pk)).hexdigest()
-            approve_url = "http://flaggy-mihirmp.dotcloud.com/approve_request?k=" + k
+            k = hashlib.sha224("%s&%s" % (f_er.pk, f_ed.pk)).hexdigest()
+            approve_url = "http://flaggy-mihirmp.dotcloud.com/approve_request?k=%s" % k
 
             try:
                 mail_status = send_mail(f_er.fname + " wants to follow you on Flaggy App!", approve_url, 'notification@flaggyapp.com', [followed_email], fail_silently=False)
 
-                if(mail_status):
+                if mail_status:
                     f = FollowPending(follower_p=f_er, following_p=f_ed, secure_key=k)
                     f.save()
-                    res = success("Request sent to " + f_ed.fname + ".")
+                    res = success("Request sent to %s." % f_ed.fname)
 
                 else:
                     res = error("Failed to send request.")
@@ -48,7 +51,7 @@ def __add_follow(follower, followed_fb, followed_email):
             # Send email about flaggy and follow request.
             try:
                 mail_status = send_mail(f_er.fname + " wants to follow you on Flaggy App!", f_er.fname + " wants you to join Flaggy.", 'notification@flaggyapp.com', [followed_email], fail_silently=False)
-                if(mail_status):
+                if mail_status:
                     res = success("New friend notified about flaggy.")
                 else:
                     res = error("Failed to notify about flaggy.")
@@ -66,13 +69,11 @@ def __unfollow(follower, followed):
     try:
         f = FollowPending.objects.get(follower_p_id=follower, following_p_id=followed)
 
-        if(f.approve):
+        if f.approve:
             f.approve = False
             follow = Follow.objects.get(follower_id=follower, following_id=followed)
-
             f.delete()
             follow.delete()
-
             return success("Successfully unfollowed.")
 
         else:
@@ -88,7 +89,7 @@ def __approve_request(k):
     try:
         req = FollowPending.objects.get(secure_key=k)
 
-        if(req.approve):
+        if req.approve:
             return error("You have already approved this request.")
 
         else:
@@ -96,7 +97,6 @@ def __approve_request(k):
             f_er = req.follower_p_id
             f_ed = req.following_p_id
             follow = Follow(follower_id=f_er, following_id=f_ed)
-
             req.save()
             follow.save()
 
@@ -109,15 +109,10 @@ def __approve_request(k):
 
 
 def __followers(u_id):
-
     try:
-        follower_list = Follow.objects.filter(following_id=u_id)
         array = {}
-        for item in follower_list:
-            dict_user = {}
-            dict_user['name'] = item.follower.fname + " " + item.follower.lname
-            array[item.follower.pk] = dict_user
-
+        for item in Follow.objects.filter(following_id=u_id):
+            array[item.follower.pk] = {'name': '%s %s' % (item.follower.fname, item.follower.lname)}
         return array
 
     except User.DoesNotExist:
@@ -126,16 +121,12 @@ def __followers(u_id):
 
 def __following(u_id):
     try:
-        following_list = Follow.objects.filter(follower_id=u_id)
-        print following_list
         array = {}
-        for item in following_list:
-            dict_user = {}
-            ##dict_user['u_id'] = item.following.pk
-            dict_user['name'] = item.following.fname + " " + item.following.lname
-            dict_user['location'] = last_check_in(item.following.pk)
-            array[item.following.pk] = dict_user
-
+        for item in Follow.objects.filter(follower_id=u_id):
+            array[item.following.pk] = {
+                'name': "%s %s" % (item.following.fname, item.following.lname),
+                'location': last_check_in(item.following.pk)
+                }
         return array
 
     except User.DoesNotExist:
@@ -144,11 +135,10 @@ def __following(u_id):
 
 def __check_in(lng, lat, u_id, comm="N/A"):
     try:
-        user = User.objects.get(pk=u_id)
         ci = CheckIn(
             longitude=lng,
             latitude=lat,
-            u_id=user,
+            u_id=User.objects.get(pk=u_id),
             when=datetime.now(),
             comment=comm)
         ci.save()
