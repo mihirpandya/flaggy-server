c437a5a611b2e09e24e119f600ba5f33dcd71172
Burak Bekdemir <bbekdemir@gmail.com>
2012-09-11 00:27:21 -0400

    use tabs instead of spaces

--- flaggy/doppio/models.py
+++ flaggy/doppio/models.py
@@ -1,32 +1,35 @@
 from django.db import models
 
+
 class User(models.Model):
-	u_id = models.AutoField(primary_key=True)
-	fname = models.CharField(max_length=30)
-	lname = models.CharField(max_length=30)
-	fb_id = models.IntegerField()
-	twitter_id = models.IntegerField()
-	email = models.EmailField(max_length=100)
-	date_joined = models.DateField()
-	distance_sensitivity = models.FloatField()
+    u_id = models.AutoField(primary_key=True)
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    fb_id = models.IntegerField()
+    twitter_id = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    date_joined = models.DateField()
+    distance_sensitivity = models.FloatField()
+
 
 class CheckIn(models.Model):
-	c_id = models.AutoField(primary_key=True)
-	longitude = models.DecimalField(max_digits = 10, decimal_places=7)
-	latitude = models.DecimalField(max_digits = 10, decimal_places=7)
-	u_id = models.ForeignKey(User)
-	when = models.DateTimeField()
-	comment = models.TextField()
+    c_id = models.AutoField(primary_key=True)
+    longitude = models.DecimalField(max_digits=10, decimal_places=7)
+    latitude = models.DecimalField(max_digits=10, decimal_places=7)
+    u_id = models.ForeignKey(User)
+    when = models.DateTimeField()
+    comment = models.TextField()
+
 
 class Follow(models.Model):
-	f_id = models.AutoField(primary_key=True)
-	follower = models.ForeignKey(User, related_name='follower')
-	following = models.ForeignKey(User, related_name='following')
+    f_id = models.AutoField(primary_key=True)
+    follower = models.ForeignKey(User, related_name='follower')
+    following = models.ForeignKey(User, related_name='following')
+
 
 class FollowPending(models.Model):
-	p_id = models.AutoField(primary_key=True)
-	follower_p = models.ForeignKey(User, related_name='follower_p')
-	following_p = models.ForeignKey(User, related_name='following_p')
-	secure_key = models.CharField(max_length=56)
-	approve = models.NullBooleanField()
-
+    p_id = models.AutoField(primary_key=True)
+    follower_p = models.ForeignKey(User, related_name='follower_p')
+    following_p = models.ForeignKey(User, related_name='following_p')
+    secure_key = models.CharField(max_length=56)
+    approve = models.NullBooleanField()
