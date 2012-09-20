e503389b9643af0d17b649714f782d8acaab3b0b
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-09-20 01:17:54 -0400

    Added prebuild

---
+++ prebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git clone https://github.com/jjguy/heatmap.py.git
+git clone https://github.com/wiredfool/Python-Imaging-Library-G4-Tiff-Support.git
+cd heatmap.py
+python setup.py install
+cd ../Python-Imaging-Library-G4-Tiff-Support
+python setup.py install
