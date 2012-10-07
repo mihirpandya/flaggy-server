90c4b6f1f32749c30694f3c094b0a1da5e34fb58
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-10-07 16:51:06 -0400

    Modified postinstall

--- static.py
+++ static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 # To import anything under django.*, we must set this variable.
-os.environ['DJANGO_SETTINGS_MODULE'] = 'flaggy2.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'flaggy.settings'
 # Import the admin module. The media directory is right under it!
 import django.contrib.admin
 # Retrieve the absolute path of the admin module.
