4021d9a89ecf4a1c143d82c491e762ea5c71399c
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-10-07 06:33:07 -0400

    Prebuild not running. Attempting to fix.

--- prebuild.sh
+++ prebuild.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt-get install graphviz libgraphviz-dev graphviz-dev python-pygraphviz
+spadeace
 git clone https://github.com/jjguy/heatmap.py.git
 git clone https://github.com/wiredfool/Python-Imaging-Library-G4-Tiff-Support.git
 cd heatmap.py
