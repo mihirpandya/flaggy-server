3a7fb310d3ffd60cae043ac27261867ec41a97da
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-11-26 20:14:09 -0500

    Changed to 30s

--- flaggy/doppio/api/notify.py
+++ flaggy/doppio/api/notify.py
@@ -86,7 +86,7 @@ def notify_check_in(u_id, lng, lat, when):
         prev_time = prev_checkin_dict['when']
         #offset_prev = prev_time
 
-        if(not too_frequent(when, prev_time, 0)): # 30 seconds since last check in
+        if(not too_frequent(when, prev_time, 30)): # 30 seconds since last check in
             payload = check_in_payload(u_id, u_id_fname, curr_checkin['lng'], curr_checkin['lat'], when)
             if push_all_followers(u_id, followers, curr_checkin, payload):
                 res = success("Sent push notifications to all followers.")
