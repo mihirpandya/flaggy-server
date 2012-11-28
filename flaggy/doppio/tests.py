e1fc1d0dc5abbbd866508b2b12f51e3321765b21
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-11-27 19:28:20 -0500

    Added unit test for follow request.

--- flaggy/doppio/tests.py
+++ flaggy/doppio/tests.py
@@ -9,6 +9,8 @@ from django.test import TestCase
 from django.test.client import Client
 from doppio.models import *
 from doppio.api.controllers import __add_user as c_add_user
+from doppio.api.controllers import __add_follow as c_add_follow
+from doppio.api.utils import follow_hash
 from random import random
 from datetime import datetime
 
@@ -17,7 +19,7 @@ class ControllerAddUserTestCase(unittest.TestCase):
 		self.fb_id = int(random()*100000000)
 		self.testUser = User(fname="Automated",
 			                 lname="Tester",
-			                 fb_id=int(random()*100000000),
+			                 fb_id=self.fb_id,
 			                 twitter_id=0,
 			                 email="me@mihirmp.com",
 			                 date_joined=datetime.now(),
@@ -29,3 +31,36 @@ class ControllerAddUserTestCase(unittest.TestCase):
 		self.assertEquals(u.fname, self.testUser.fname)
 		self.assertEquals(u.lname, self.testUser.lname)
 		self.assertEquals(u.email, self.testUser.email)
+
+class ControllerFollowTestCase(unittest.TestCase):
+	def setUp(self):
+		self.fb_id1 = int(random()*100000000)
+		self.fb_id2 = int(random()*100000000)
+		self.testUser1 = User(fname="Automated1",
+			                 lname="Tester1",
+			                 fb_id=self.fb_id1,
+			                 twitter_id=0,
+			                 email="one@mihirmp.com",
+			                 date_joined=datetime.now(),
+			                 distance_sensitivity=1)
+		self.testUser2 = User(fname="Automated2",
+			                 lname="Tester2",
+			                 fb_id=self.fb_id2,
+			                 twitter_id=0,
+			                 email="two@mihirmp.com",
+			                 date_joined=datetime.now(),
+			                 distance_sensitivity=1)
+		self.testUser1.save()
+		self.testUser2.save()
+		self.id_1 = self.testUser1.u_id
+		self.id_2 = self.testUser2.u_id
+		self.hash = follow_hash(self.id_1, self.id_2)
+
+	def testFollowRequest(self):
+		c_add_follow(self.id_1, self.fb_id2)
+		try:
+			u = FollowPending.objects.get(follower_p_id=self.id_1, following_p_id=self.id_2)
+			self.assertEquals(u.hash, self.hash)
+			self.assertEquals(u.approve, None)
+		except:
+			False
